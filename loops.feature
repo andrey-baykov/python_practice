b2dbc5e7aaf9e35a17c2954b29c6b46b71c21cd3
andrey-baykov <baykov.andrey@gmail.com>
2022-08-20 22:13:28 -0700

    Added verifying two types of pages. Added "Find item throw menus and validate that" scenario

--- loops.feature
+++ loops.feature
@@ -28,3 +28,43 @@ Feature: Loops and tables
         | Home & garden | Tools & Workshop Equipment               | Tools & Workshop Equipment                |
         | Home & garden | Home Improvement                         | Home Improvement                          |
         | Home & garden | Kitchen, Dining & Bar Supplies           | Kitchen, Dining & Bar Supplies            |
+
+  Scenario: Find item throw search with parameters in the left menu and validate that
+    Given Open URL "ebay.com"
+    Then Input "DJI" in the search field
+    And Click search button
+    And Apply left menu with filters from the table
+      | Filter menu | Filter              |
+      | Brand       | DJI                 |
+      | Model       | DJI Mavic Pro       |
+      | Type        | Ready to Fly Drone  |
+      | Condition   | New                 |
+    And Validate filters was applied and shows above result table
+      | Filter              |
+      | DJI                 |
+      | DJI Mavic Pro       |
+      | Ready to Fly Drone  |
+      | New                 |
+    Then Validate item with parameters from the table
+      | Apply | Parameters           | Value    |
+      | Yes   | Description contains | Drone    |
+      | Yes   | Shipping             | Free     |
+      | Yes   | Returns              | Free     |
+      | No    | Price lower then     | 400      |
+      | No    | Price higher then    | 30       |
+      | Yes   | Price between        | 30 400   |
+
+  Scenario: Find item throw menus and validate that
+    Given Open URL "ebay.com"
+    Then Click flying out menu "Shop by category" and verify correct page was opened
+        | Category      | Subcategory      | Page title       |
+        | Electronics   | Cameras & Photo  | Cameras & Photo  |
+    And Click item "Digital Cameras" in mosaic menu
+    And Verify page "Digital Cameras" was opened
+    Then Click item "Hasselblad" in carousel menu
+    And Verify page "Hasselblad Photo Digital Cameras" was opened
+    Then Validate item with parameters from the table
+      | Apply | Parameters           | Value      |
+      | Yes   | Description contains | Hasselblad |
+      | Yes   | Price higher then    | 1500     |
+      | Yes   | Price between        | 1800 2000   |
--- steps/defs_loops.py
+++ steps/defs_loops.py
@@ -3,7 +3,7 @@ from time import sleep
 
 from behave import step
 from selenium import webdriver
-from selenium.common import NoSuchElementException, ElementNotInteractableException
+from selenium.common import NoSuchElementException, ElementNotInteractableException, ElementClickInterceptedException
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
@@ -65,11 +65,9 @@ def verify_page_by_title(context, page_title) -> None:
 
     :param context: self
     :param page_title: page title to verify
-    :return: None
+    :return: Exception if Failed, None if Pass
     """
     path_found = True
-    # title_path = None
-    page_to_verify = None
     try:
         title_path = f"//div[@class='navigation-desktop']//a[text()='{page_title}']"
         context.driver.find_element(By.XPATH, title_path)
@@ -123,5 +121,214 @@ def flying_out_menu_test(context) -> None:
             test_pass = False
         except ElementNotInteractableException:
             warnings.warn(f"Path {subcategory_item_path} not found or page title '{page_title}' incorrect.")
+            test_pass = False
 
     assert test_pass, 'Test failed. Read warnings.'
+
+
+@step('Input "{search_string}" in the search field')
+def input_search_string(context, search_string) -> None:
+    """Function fill out string to search in to search field
+
+    :param context: self
+    :param search_string:  string to insert in to search field
+    :return: None
+    """
+    search_string_field_path = f"//input[@aria-label='Search for anything']"
+    search_string_field = context.driver.find_element(By.XPATH, search_string_field_path)
+    search_string_field.send_keys(search_string)
+    sleep(1)
+
+
+@step('Click search button')
+def click_search_button(context):
+    """Click on the search button
+
+    :param context: self
+    :return: None
+    """
+    search_button_path = f"//input[@id='gh-btn' and @value='Search']"
+    search_button = context.driver.find_element(By.XPATH, search_button_path)
+    search_button.click()
+    sleep(2)
+
+
+@step('Apply left menu with filters from the table')
+def apply_left_menu_filter(context):
+    """Applies filter from left menu. Filter menu and filter from context.table
+
+    :param context: self
+    :return: None
+    """
+    for row in context.table.rows:
+        filter_menu_name, filter_item_name = row
+        aria_expanded_path = f"//ul[@class='x-refine__left__nav']//div[./h3[text()='{filter_menu_name}']]"
+        aria_expanded = context.driver.find_element(By.XPATH, aria_expanded_path)
+        if not aria_expanded.get_attribute("aria-expanded"):
+            filter_menu_path = f"//ul[@class='x-refine__left__nav']//h3[text()='{filter_menu_name}']"
+            filter_menu = context.driver.find_element(By.XPATH, filter_menu_path)
+            filter_menu.click()
+            sleep(1)
+
+        filter_item_path = f"//li[@class='x-refine__main__list '][.//h3[text()='{filter_menu_name}']]" \
+                           f"//span[text()='{filter_item_name}']"
+        filter_item = context.driver.find_element(By.XPATH, filter_item_path)
+        filter_item.click()
+        sleep(2)
+
+
+@step('Validate filters was applied and shows above result table')
+def validate_filter_above_result_table(context) -> bool:
+    """Validate that filters above the results table are present. Filters data in one column
+    table.
+
+    :param context: self
+    :return: True if Pass, False if Failed
+    """
+    verify_result = False
+    for num, row in enumerate(context.table.rows, 1):
+        filter_data = row[0]
+        try:
+            filter_path = f"//div[@class ='carousel__viewport'][.// ul[@class ='carousel__list']]" \
+                          f"// div[text()='{filter_data}']"
+            context.driver.find_element(By.XPATH, filter_path)
+            verify_result = True
+            print(f'{num}. Filter "{filter_data}" - pass.')
+        except NoSuchElementException:
+            warnings.warn(f'Filter "{filter_data}" not found')
+            verify_result = False
+
+    assert verify_result, "Verify filters are failed"
+
+
+@step('Validate item with parameters from the table')
+def validate_item(context):
+    test_set = {}
+    test_set_pass = []
+    for row in context.table.rows:
+        apply, parameter, value = row
+        if apply == "Yes":
+            test_set[parameter] = value
+    for test, test_value in test_set.items():
+        if test in ("Description contains", "Shipping", "Returns"):
+            try:
+                element = context.driver.find_element(By.XPATH, get_xpath_type_1(test))
+            except NoSuchElementException:
+                warnings.warn("Screen type 2")
+                element = context.driver.find_element(By.XPATH, get_xpath_type_2(test))
+            if test_value in element.text:
+                print(f'Test "{test}" with value "{test_value}" in "{element.text}"- pass.')
+                test_set_pass.append(True)
+            else:
+                warnings.warn(f'Test "{test}" with value "{test_value}" in "{element.text}" - fail.')
+                test_set_pass.append(False)
+        elif "Price" in test:
+            try:
+                element = context.driver.find_element(By.XPATH, get_xpath_type_1(test))
+            except NoSuchElementException:
+                warnings.warn("Screen type 2")
+                element = context.driver.find_element(By.XPATH, get_xpath_type_2(test))
+            web_price = float(element.text[1:].replace(",", ""))
+            if 'lower' in test:
+                if web_price < float(test_value):
+                    print(f'Test "{test}". Web price "{web_price}" lower then "{float(test_value)}" - pass.')
+                    test_set_pass.append(True)
+                else:
+                    warnings.warn(f'Test "{test}". Web price "{web_price}" higher or equal then "{float(test_value)}"'
+                                  f' - fail.')
+                    test_set_pass.append(False)
+            elif 'higher' in test:
+                if web_price > float(test_value):
+                    print(f'Test "{test}". Web price "{web_price}" higher then "{float(test_value)}" - pass.')
+                    test_set_pass.append(True)
+                else:
+                    warnings.warn(f'Test "{test}". Web price "{web_price}" lower or equal then "{float(test_value)}"'
+                                  f' - fail.')
+                    test_set_pass.append(False)
+            else:
+                min_value, max_value = test_value.split()
+                if float(min_value) <= web_price <= float(max_value):
+                    print(f'Test "{test}". Web price "{web_price}" between "{float(min_value)}" and '
+                          f'"{float(max_value)}" - pass.')
+                    test_set_pass.append(True)
+                else:
+                    warnings.warn(f'Test "{test}". Web price "{web_price}" not between "{float(min_value)}" and '
+                                  f'"{float(max_value)}" - fail.')
+                    test_set_pass.append(False)
+
+    assert all(test_set_pass), "Test set failed. See log."
+
+
+def get_xpath_type_1(parameter) -> str:
+    """Function return XPATH for provided parameter
+
+    :param parameter: parameter for validate
+    :return:
+    """
+    if parameter == "Description contains":
+        output = f"//li[@class='s-item s-item__pl-on-bottom s-item--watch-at-corner']" \
+                 f"//h3[@class='s-item__title']"
+    elif parameter == "Shipping":
+        output = f"//li[@class='s-item s-item__pl-on-bottom s-item--watch-at-corner']" \
+                 f"//*[@class='s-item__shipping s-item__logisticsCost']"
+    elif parameter == "Returns":
+        output = f"//li[@class='s-item s-item__pl-on-bottom s-item--watch-at-corner']" \
+                 f"//*[@class='s-item__free-returns s-item__freeReturnsNoFee']"
+    elif parameter in ("Price lower then", "Price higher then", "Price between"):
+        output = f"//li[@class='s-item s-item__pl-on-bottom s-item--watch-at-corner']" \
+                 f"//*[@class='s-item__price']"
+    else:
+        output = "No xpath for particular parameter."
+    return output
+
+
+def get_xpath_type_2(parameter) -> str:
+    """Function return XPATH for provided parameter
+
+    :param parameter: parameter for validate
+    :return:
+    """
+    if parameter == "Description contains":
+        output = f"//div[@class='s-item__wrapper clearfix']//h3[contains(@class, 's-item__title')]"
+    elif parameter == "Shipping":
+        output = f"//div[@class='s-item__wrapper clearfix']" \
+                 f"//*[@class='s-item__shipping s-item__logisticsCost']"
+    elif parameter == "Returns":
+        output = f"//div[@class='s-item__wrapper clearfix']" \
+                 f"//*[@class='s-item__free-returns s-item__freeReturnsNoFee']"
+    elif parameter in ("Price lower then", "Price higher then", "Price between"):
+        output = f"//div[@class='s-item__wrapper clearfix']" \
+                 f"//*[@class='s-item__price']"
+    else:
+        output = "No xpath for particular parameter."
+    return output
+
+
+@step('Click item "{menu_item}" in mosaic menu')
+def choose_mosaic_menu(context, menu_item):
+    menu_items_list_path = f"//div[@class='b-visualnav__grid']//div[@class='b-visualnav__title']"
+    menu_items_list = context.driver.find_elements(By.XPATH, menu_items_list_path)
+    for item in menu_items_list:
+        if item.text == menu_item:
+            item.click()
+            break
+    sleep(3)
+
+
+@step('Click item "{menu}" in carousel menu')
+def choose_in_carousel_menu(context, menu):
+    menu_item_path = f"//div[@class='carousel__viewport carousel__viewport--mask'][./ul[@class='carousel__list']]" \
+                     f"//li[.//p[text()='{menu}']]"
+    menu_item = context.driver.find_element(By.XPATH, menu_item_path)
+    button_right_path = f"//div[./div[@class='carousel__viewport carousel__viewport--mask']" \
+                        f"[./ul[@class='carousel__list']][.//li[.//p[text()='{menu}']]]]" \
+                        f"//button[@aria-label='Next Slide']"
+    button_right = context.driver.find_element(By.XPATH, button_right_path)
+    for _ in range(3):
+        try:
+            menu_item.click()
+        except ElementClickInterceptedException:
+            button_right.click()
+            sleep(3)
+            warnings.warn('Next Slide click')
+    sleep(3)
