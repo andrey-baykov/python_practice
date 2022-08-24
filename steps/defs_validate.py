1be521837fcee9b08fe53786654039ab3dad67bf
andrey-baykov <baykov.andrey@gmail.com>
2022-08-24 01:57:05 -0700

    Added browser modes: regular or incognito

--- steps/defs_validate.py
+++ steps/defs_validate.py
@@ -8,14 +8,35 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 
+@step('Set regular or incognito mode for browser: {mode}')
+def set_browser_mode(context, mode) -> None:
+    """
+    Set browser to regular or incognito mode for all scenarios.
+    :param context: mode
+    :param mode: regular - open browser in regular, incognito - open browser in incognito mode.
+    :return: None
+    """
+    if mode in ('regular', 'incognito'):
+        context.browser_mode = mode
+    else:
+        raise Exception('Incorrect browser mode. Allow only regular or incognito mode.')
+
+
 @step('Open a home page on the eBay website')
 def open_browser(context) -> None:
     """
-    Function create an instance of browser with URL 'https://www.eBay.com/'
-    :param context: driver
+    Function create an instance of browser with URL 'https://www.eBay.com/'\n
+    :param context: driver, browser_mode
     :return: None
     """
-    context.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    if context.browser_mode == 'incognito':
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--incognito")
+        context.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),
+                                          chrome_options=chrome_options)
+    else:
+        context.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
     context.driver.get("https://www.eBay.com/")
 
 
@@ -27,8 +48,14 @@ def type_string_to_search(context, string_to_search) -> None:
     :param string_to_search: string to search
     :return: None
     """
-    search_string_path = "//input[@aria-label='Search for anything']"
-    search_string = context.driver.find_element(By.XPATH, search_string_path)
+    try:
+        search_string_path = "//input[@aria-label='Search for anything']"
+        search_string = context.driver.find_element(By.XPATH, search_string_path)
+    except NoSuchElementException:
+        search_string_path = "//fieldset[./legend[text()='Enter keywords or item number']]" \
+                             "//input[@placeholder='Enter keywords or item number']"
+        search_string = context.driver.find_element(By.XPATH, search_string_path)
+
     search_string.send_keys(string_to_search)
     sleep(1)
 
@@ -40,8 +67,14 @@ def click_button_search(context) -> None:
     :param context: driver
     :return: None
     """
-    button_search_path = "//input[@id='gh-btn'][@value='Search']"
-    button_search = context.driver.find_element(By.XPATH, button_search_path)
+    try:
+        button_search_path = "//input[@id='gh-btn'][@value='Search']"
+        button_search = context.driver.find_element(By.XPATH, button_search_path)
+    except NoSuchElementException:
+        button_search_path = "//fieldset[./legend[text()='Enter keywords or item number']]" \
+                             "//button[text()='Search']"
+        button_search = context.driver.find_element(By.XPATH, button_search_path)
+
     button_search.click()
     sleep(2)
 
@@ -63,7 +96,7 @@ def apply_filters_from_table(context) -> None:
                             f"//*[text()='{category_filter}']"
         items_filter = context.driver.find_element(By.XPATH, items_filter_path)
         items_filter.click()
-        sleep(2)
+        sleep(5)
 
 
 @step('Validate that items in the result list matched to filters')
@@ -95,9 +128,64 @@ def validate_items_in_result_list(context) -> None:
 
     result_o = len(results_list_origin)
     result_f = len(results_list_filtered)
-    print(results_list_origin_path)
-    print(results_list_filtered_path)
-    print(result_o)
     sleep(5)
     assert result_o == result_f, f"Validate is failed. Filtered results {result_f} items are not equal {result_o} " \
                                  f"items of original results."
+
+
+@step('Choose in dropdown list "{menu_item}"')
+def cats(context, menu_item) -> None:
+    """
+    Choose an element in the category dropdown list in the search string.
+    :param context: driver
+    :param menu_item: category in the dropdown list
+    :return: None
+    """
+    dropdown_menu_path = "//select[@id='gh-cat']"
+    cat = context.driver.find_element(By.XPATH, dropdown_menu_path)
+    cat.click()
+    sleep(1)
+
+    menu_cat_path = f"//select[@id='gh-cat']/option[text()='{menu_item}']"
+    art_opt = context.driver.find_element(By.XPATH, menu_cat_path)
+    art_opt.click()
+    sleep(1)
+
+
+@step('Click "Advanced search" link')
+def click_advanced_search_link(context) -> None:
+    """
+    Click a link "Advanced Search"
+    :param context: driver
+    :return: None
+    """
+    adv_search_link_path = "//a[@aria-label='Advanced Search']"
+    adv_search_link = context.driver.find_element(By.XPATH, adv_search_link_path)
+    adv_search_link.click()
+    sleep(2)
+
+
+@step('Verify "Advanced search" page was opened')
+def verify_adv_page(context) -> None:
+    """
+    Validate current opened page is "Advanced Search"
+    :param context: driver
+    :return: None
+    """
+    adv_page_path = "//div[@id='AreaTitle']//*[text()='Advanced Search']"
+    adv_page = context.driver.find_element(By.XPATH, adv_page_path)
+    assert adv_page.text == 'Advanced Search', "Present page isn't Advanced Search page"
+
+
+@step('Choose parameters on the page')
+def choose_param_adv_page(context) -> None:
+    """
+    Choose provided in the table parameters for page 'Advanced search'
+    :param context: driver, table
+    :return: None
+    """
+    for item in context.table:
+        param_path = f"//*[normalize-space() = '{item['parameter']}']"
+        param = context.driver.find_element(By.XPATH, param_path)
+        param.click()
+        sleep(2)
--- validate.feature
+++ validate.feature
@@ -1,5 +1,11 @@
 Feature: Validate searched results
 
+  # For some reason when I run all tests in regular mode I have browser exception
+  # "Too many redirections..."
+  # Then I switch to incognito mode and continue execute tests without exceptions
+  Background:
+    Given Set regular or incognito mode for browser: regular
+
 # In this scenario page divider is present
   Scenario: Find list of items "360 action camera" and validate that match to search
     Given Open a home page on the eBay website
@@ -29,3 +35,36 @@ Feature: Validate searched results
       | filters              |
       | Apple Watch Series 7 |
       | 45                   |
+
+  Scenario: Find Rammstein tickets in LA and validate result
+    Given Open a home page on the eBay website
+    Then Type "Rammstein" in the search string
+    And Choose in dropdown list "Tickets & Experiences"
+    And Click search button
+    Then Apply filters to results
+      | category | filter |
+      | Category | Concert Tickets |
+    Then Apply filters to results
+      | category | filter |
+      | Venue City | Los Angeles |
+    And Validate that items in the result list matched to filters
+      | filters |
+      | Rammstein |
+      | Los Angeles |
+
+  Scenario: Advanced search Rammstein tickets in LA
+    Given Open a home page on the eBay website
+    Then Click "Advanced search" link
+    And Verify "Advanced search" page was opened
+    Then Type "Rammstein concert Los Angeles" in the search string
+    And Choose parameters on the page
+      | parameter             |
+      | Title and description |
+      | Buy It Now            |
+    And Click search button
+    And Validate that items in the result list matched to filters
+      | filters |
+      | Rammstein |
+      | Los Angeles |
+
+
